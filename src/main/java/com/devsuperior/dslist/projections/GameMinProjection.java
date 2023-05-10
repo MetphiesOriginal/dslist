94105de45138b4b118998f2efb9e5f0816936120
Matheus Mariano <odink1337@gmail.com>
2023-05-09 23:34:04 -0300

    Consulta SQL, projection, Search games by list

---
+++ src/main/java/com/devsuperior/dslist/projections/GameMinProjection.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslist.projections;
+
+public interface GameMinProjection {
+	
+	Long GetID();
+	String getTitle();
+	Integer getYear();
+	String getImgUrl();
+	String getShortDescription();
+	Integer getPosition();
+	Long getId();
+	
+}
