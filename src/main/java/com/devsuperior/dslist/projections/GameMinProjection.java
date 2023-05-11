32347927d6f8f6cd4562e75aa9f6b4c84efe978b
Matheus Mariano <odink1337@gmail.com>
2023-05-11 00:06:16 -0300

    Homolog ok

--- src/main/java/com/devsuperior/dslist/projections/GameMinProjection.java
+++ src/main/java/com/devsuperior/dslist/projections/GameMinProjection.java
@@ -4,7 +4,7 @@ public interface GameMinProjection {
 	
 	Long GetID();
 	String getTitle();
-	Integer getYear();
+	Integer getGameYear();
 	String getImgUrl();
 	String getShortDescription();
 	Integer getPosition();
